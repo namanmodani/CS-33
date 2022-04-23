a2ea6dcb7e8cb822912195e20445d23682372a2f
Naman Modani <modani.naman@gmail.com>
2022-04-22 20:05:09 -0700

    complete switch_prob.c

--- Homework/Homework 3/switch_prob.c
+++ Homework/Homework 3/switch_prob.c
@@ -1,3 +1,27 @@
 // CS 33 Homework 3
 // Naman Modani
 // switch_prob.c
+
+long switch_prob(long x, long n)
+{
+    long result = x;
+    switch (n)
+    {
+        case 60:
+        case 62:
+            result = 8 * x;
+            break;
+        case 63:
+            result >>= 3;
+            break;
+        case 64:
+            result <<= 4;
+            x = result - x;
+        case 65:
+            x *= x;
+        default:
+            result = x + 75;
+            break;
+    }
+    return result;
+}
