56fb65e7e01296585bbde6c2341cf1b38b8f2329
Naman Modani <modani.naman@gmail.com>
2022-05-10 21:22:51 -0700

    phase 3 completed

---
+++ Labs/Attack Lab/target89/phase3.d
@@ -0,0 +1,9 @@
+
+phase3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 b0 2f 62 55 	mov    $0x55622fb0,%rdi
+   7:	c3                   	retq   
---
+++ Labs/Attack Lab/target89/phase3.s
@@ -0,0 +1,2 @@
+movq    $0x55622fb0,%rdi
+retq
