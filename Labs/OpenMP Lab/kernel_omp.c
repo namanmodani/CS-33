d274783b50fbcf7febe3967d20e218d0c96bb982
Naman Modani <modani.naman@gmail.com>
2022-06-02 20:43:13 -0700

    completed openmp lab sans report, 4.2x

--- Labs/OpenMP Lab/kernel_omp.c
+++ Labs/OpenMP Lab/kernel_omp.c
@@ -6,119 +6,81 @@
  */
 
 #include <omp.h>
+#include <stdio.h>
+
 #include "kernel.h"
 
-void kernel_omp(int* input, int* ref, int64_t rows, int64_t cols, int penalty) {
-  int64_t blockedColumns = cols / 32;
-  int64_t diagonalCount = (rows / 32) + blockedColumns - 1;
-  int64_t upwardDiagonalCount = (diagonalCount + 1) / 2;
-  int64_t diag = 0;
+void kernel_omp(int *input, int *ref, int64_t rows, int64_t cols, int penalty) 
+{
+    int blockSize = 64;
+    int unrollType = 8;
+    
+    for (int x = 1; x < 2 * rows - blockSize; x += blockSize) 
+    {
+        int overflow = (x >= rows) ? (x - rows + blockSize) : 0;
+        #pragma omp parallel for
+        for (int y = overflow; y <= x - overflow; y += blockSize) 
+        {
+            int a = x - y;
+            int b = y + 1;
+            for (int i = a; i < a + blockSize; i++) 
+            {
+                for (int j = b; j < b + blockSize; j += unrollType) 
+                {
+                    int64_t idx = i * cols + j;
+                    int64_t idxNW = idx - cols - 1;
+                    int64_t idxN = idx - cols;
+                    int64_t idxW = idx - 1;
+                    int r = ref[idx];
 
-  for (; diag != 24; ++diag) {
-    for (int64_t col = 0; col != diag + 1; ++col) {
-      int startRow = 1 + 32 * (diag - col);
-      int startColumn = 1 + 32 * col;
-      int64_t endRow = startRow + 32;
-      int64_t endColumn = startColumn + 32;
+                    int inputNW = input[idxNW];
+                    int inputW = input[idxW];
+                    int inputN = input[idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
 
-      int64_t index;
-      int64_t indexNW;
-      int64_t indexN;
-      int64_t indexW;
-  
-      int r;
-      int inputNW;
-      int inputW;
-      int inputN;
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
 
-      for (; startRow != endRow; ++startRow) {
-        for (int64_t col = startColumn; col != endColumn; ++col) {
-          index = startRow * cols + col;
-          indexNW = index - cols - 1;
-          indexN = index - cols;
-          indexW = index - 1;
-      
-          r = ref[index];
-          inputNW = input[indexNW];
-          inputW = input[indexW];
-          inputN = input[indexN];
-      
-          input[index] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+
+                    r = ref[++idx];
+                    inputNW = input[++idxNW];
+                    inputW = input[++idxW];
+                    inputN = input[++idxN];
+                    input[idx] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
+                }
+            }
         }
-      }
-            
     }
-  }
-
-  for (; diag != upwardDiagonalCount; ++diag) {
-    #pragma omp parallel for default(none) firstprivate(input, ref, cols, diag, penalty)
-    for (int64_t col = 0; col < diag + 1; ++col) {
-      int startRow = 1 + 32 * (diag - col);
-      int startColumn = 1 + 32 * col;
-      int64_t endRow = startRow + 32;
-      int64_t endColumn = startColumn + 32;
-
-      int64_t index;
-      int64_t indexNW;
-      int64_t indexN;
-      int64_t indexW;
-  
-      int r;
-      int inputNW;
-      int inputW;
-      int inputN;
-
-      for (; startRow != endRow; ++startRow) {
-        for (int64_t col = startColumn; col != endColumn; ++col) {
-          index = startRow * cols + col;
-          indexNW = index - cols - 1;
-          indexN = index - cols;
-          indexW = index - 1;
-      
-          r = ref[index];
-          inputNW = input[indexNW];
-          inputW = input[indexW];
-          inputN = input[indexN];
-      
-          input[index] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
-        }
-      }
-    }
-  }
-
-  for (; diag != diagonalCount; ++diag) {
-    #pragma omp parallel for default(none) firstprivate(input, ref, cols, diag, penalty, upwardDiagonalCount, blockedColumns)
-    for (int64_t col = diag - upwardDiagonalCount + 1; col < blockedColumns; ++col) {
-      int startRow = 1 + 32 * (diag - col);
-      int startColumn = 1 + 32 * col;
-      int64_t endRow = startRow + 32;
-      int64_t endColumn = startColumn + 32;
-
-      int64_t index;
-      int64_t indexNW;
-      int64_t indexN;
-      int64_t indexW;
-  
-      int r;
-      int inputNW;
-      int inputW;
-      int inputN;
-
-      for (; startRow != endRow; ++startRow) {
-        for (int64_t col = startColumn; col != endColumn; ++col) {
-          index = startRow * cols + col;
-          indexNW = index - cols - 1;
-          indexN = index - cols;
-          indexW = index - 1;
-      
-          r = ref[index];
-          inputNW = input[indexNW];
-          inputW = input[indexW];
-          inputN = input[indexN];
-      
-          input[index] = maximum(inputNW + r, inputW - penalty, inputN - penalty);
-        }
-      }
-    }
-  }
 }
